6a45036354155a467a1bb81d2ae5498ee34d404c
DCoelho28 <149810523+DCoelho28@users.noreply.github.com>
2024-10-19 09:27:57 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -8,118 +8,90 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-#data to use from the X_train.csv file
+# data to use from the X_train.csv file
 data = pd.read_csv('./X_train.csv')
 
-print(f"data set size: {len(data)}")
 # data clean up, removing the big amount of uneccessary 0.0
-data_clean = data[(data['x_1'] != 0.0) | (data['y_1'] != 0.0) |
+dataClean = data[(data['x_1'] != 0.0) | (data['y_1'] != 0.0) |
             (data['x_2'] != 0.0) | (data['y_2'] != 0.0) |
             (data['x_3'] != 0.0) | (data['y_3'] != 0.0)]
-print(f"data_clean set size: {len(data_clean)}")
 
-output_path = ('./data.csv')
-# data_clean.to_csv(output_path,index=False)
+# comecaIndex is a stack with all the initial positions (where t == 0.0)
+# since the uneccessary 0.0 were deleted, this is grouping only the initial positions
+comecaIndex = np.hstack((dataClean[dataClean.t == 0.0].index.values))
 
-# initial positions -> t=0.0
-initial_positions = data_clean[(data_clean['t'] == 0.0)]
-print(f"initial_positions set size: {len(initial_positions)}")
+# acabaIndex is a stack with all the final positions (where t == 0.0, but - 1 because this way is getting the row before the initial positions, that corresponds to the final positions)
+# in the final is adding the last line, because the last final positions does not have in front an initial position
+# since the uneccessary 0.0 were deleted, this is grouping only the initial positions
+acabaIndex = np.hstack((dataClean[dataClean.t == 0.0].index.values - 1, dataClean.iloc[-1]['Id']))
 
-# group by positions to have unique initial positions
-unique_initial_positions = initial_positions.drop_duplicates(subset=['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3'])
-
-output_path = ('./unique_initial_positions.csv')
-# unique_initial_positions.to_csv(output_path,index=False)
-
-print(f"unique_initial_positions set size: {len(unique_initial_positions)}")
-
-
-comecaIndex = np.hstack((data_clean[data_clean.t == 0.0].index.values))
-print(comecaIndex)
-acabaIndex = np.hstack((data_clean[data_clean.t == 0.0].index.values - 1, data_clean.iloc[-1]['Id']))
-print(acabaIndex)
-
-comeca_as_DataFrame = pd.DataFrame({'comeca': comecaIndex})
-# output_path = ('./comeca.csv')
-comeca_as_DataFrame.to_csv(output_path,index=False)
-
-data_clean.loc[:, ['x1_initial_position', 'y1_initial_position', 'x2_initial_position', 'y2_initial_position', 'x3_initial_position', 'y3_initial_position']] = 0.0
+# add to dataClean the initial positions corresponding to each position from each iteration
+# add the initial positions from where the iteration starts until where the iteration ends
+dataClean.loc[:, ['x1_initial_position', 'y1_initial_position', 'x2_initial_position', 'y2_initial_position', 'x3_initial_position', 'y3_initial_position']] = 0.0
 #completed_data = pd.DataFrame()
 for j in range(comecaIndex.size):
-    data_clean.loc[comecaIndex[j]:acabaIndex[j+1], ['x1_initial_position', 'y1_initial_position', 'x2_initial_position', 'y2_initial_position', 'x3_initial_position', 'y3_initial_position']] = data.loc[comecaIndex[j], ['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']].values 
+    dataClean.loc[comecaIndex[j]:acabaIndex[j+1], ['x1_initial_position', 'y1_initial_position', 'x2_initial_position', 'y2_initial_position', 'x3_initial_position', 'y3_initial_position']] = data.loc[comecaIndex[j], ['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']].values 
 
-#print("------------------")
-print(f"data_clean set size: {len(data_clean)}")
+# drop the velocities, since it's not being used
+dataClean = dataClean.drop(columns=['v_x_1', 'v_y_1', 'v_x_2', 'v_y_2', 'v_x_3', 'v_y_3'], inplace=False)
+# it's not necessary to have the initial positions
+# the solution wants to predict positions where t > 0.0
+dataClean = dataClean[(dataClean['t'] != 0.0)]
+# asserting the Id column
+colunaId = dataClean.pop('Id')
+dataClean.insert(0, 'Id', colunaId)
 
-data_clean = data_clean.drop(columns=['v_x_1', 'v_y_1', 'v_x_2', 'v_y_2', 'v_x_3', 'v_y_3'], inplace=False)
-# a prof diz que t > 0.0
-data_clean = data_clean[(data_clean['t'] != 0.0)]
-coluna_Id = data_clean.pop('Id')
-data_clean.insert(0, 'Id', coluna_Id) #Isto pq?
+# 80% to train, 20% to val
+train, val = train_test_split(dataClean, test_size=0.2, random_state=42)
 
-output_path = ('./data_clean.csv')
-# data_clean.to_csv(output_path,index=False)
-
-#80% to train, 10% to test, 10% to val
-train, val = train_test_split(data_clean, test_size=0.2, random_state=42)
-
+# get the X_test and change the name of the columns
 test = pd.read_csv('./X_test.csv')
 cols = ['Id','t','x1_initial_position', 'y1_initial_position', 'x2_initial_position', 'y2_initial_position', 'x3_initial_position', 'y3_initial_position']
 test.columns=cols
 
-print(f"Train set size: {len(train)}")
-print(f"Validation set size: {len(val)}")
-print(f"Test set size: {len(test)}")
-print(f"Total before clean =  1285002")
+###     END OF TASK 1.1     ###
 
-################################################################################################
+# features are the columns of entrance, to get the final positions, depending on the time and initial positions (that are the features)
+featuresX = ['x1_initial_position', 'y1_initial_position', 'x2_initial_position', 'y2_initial_position', 'x3_initial_position', 'y3_initial_position', 't']
 
-#dados de entrada
-features_X = ['x1_initial_position', 'y1_initial_position', 'x2_initial_position', 'y2_initial_position', 'x3_initial_position', 'y3_initial_position', 't']
-#o que queremos prever 
+# y are the final positions, calculated depending on the initial positions and time
 y = ['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']
 
-entry_train = train[features_X]
-output_train = train[y]
+# assigning the inputs and outputs
+entryTrain = train[featuresX]
+outputTrain = train[y]
+entryVal = val[featuresX]
+outputVal = val[y]
 
-entry_test  = test[features_X]
-# output_test = test[y]
+entryTest  = test[featuresX]
 
-entry_val = val[features_X]
-output_val = val[y]
-
-#Pipeline pedida pela prof com standardscaler (padronização dos dados) e linear regression
+# Pipeline with StandardScaler and LinearRegression
 pipeline = Pipeline([('scaler', StandardScaler()),
              ('linear_regression', LinearRegression())])
 
-pipeline.fit(entry_train, output_train)
+pipeline.fit(entryTrain, outputTrain)
 
-output_train_prediction = pipeline.predict(entry_train)
-output_val_prediction = pipeline.predict(entry_val)
-output_test_prediction = pipeline.predict(entry_test)
+outputTrainPrediction = pipeline.predict(entryTrain)
+outputValPrediction = pipeline.predict(entryVal)
+
+outputTestPrediction = pipeline.predict(entryTest)
 
 columns = ['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']
-# Create a DataFrame from the predicted values
-df_output = pd.DataFrame(output_test_prediction, columns=columns)
-# Add a column for the 'id' values
-df_output['id'] = df_output.index
-# Reorder the columns to match the desired format
-df_output = df_output[['id', 'x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']]
-#df_output = pd.DataFrame({'predicted_values': output_test_prediction})
-output_path = ('./baseline-model.csv')
-df_output.to_csv(output_path,index=False)
-#output_path = r'C:\Users\vidcoelh\Documents\Pessoal\AA\output_test_prediction.csv'
-#output_test_prediction.to_csv(output_path,index=False)
+# create a DataFrame from the predicted values
+df = pd.DataFrame(outputTestPrediction, columns=columns)
+# add a column for the 'id' and reorder the columns to have the output file (submission file) in the expected structure
+df['id'] = df.index
+df = df[['id', 'x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']]
 
-rmse_train =  math.sqrt(mean_squared_error(output_train, output_train_prediction))
-rmse_val = math.sqrt(mean_squared_error(output_val, output_val_prediction))
-# rmse_test = math.sqrt(mean_squared_error(output_test, output_test_prediction))
+# extract the submission file
+outputPath = ('./baseline-model.csv')
+df.to_csv(outputPath,index=False)
 
-#se o MSE do dado de treino for muito menor que o MSE de validação e de treino --> modelo está a sofrer overfitting (modelo ajusta-se demasiado aos dados de treino)
-print(f"Train RMSE: {rmse_train}")
-print(f"Validation RMSE: {rmse_val}")
-# print(f"Test RMSE: {rmse_test}")
+# calculate rmse values 
+rmse_train =  math.sqrt(mean_squared_error(outputTrain, outputTrainPrediction))
+rmse_val = math.sqrt(mean_squared_error(outputVal, outputValPrediction))
 
+# plot
 def plot_y_yhat(y_val,y_pred, plot_title = "plot"):
     labels = ['x_1','y_1','x_2','y_2','x_3','y_3']
     MAX = 500
@@ -141,5 +113,5 @@ def plot_y_yhat(y_val,y_pred, plot_title = "plot"):
     plt.show()
     
     
-plot_y_yhat(np.array(output_val), output_val_prediction, plot_title="y_yhat_val")
-plot_y_yhat(np.array(output_train), output_train_prediction, plot_title="y_yhat_test")
+plot_y_yhat(np.array(outputVal), outputValPrediction, plot_title="y_yhat_val")
+plot_y_yhat(np.array(outputTrain), outputTrainPrediction, plot_title="y_yhat_train")
